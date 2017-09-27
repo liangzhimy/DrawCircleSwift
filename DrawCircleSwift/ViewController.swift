dd95b9d3768397f9ff7987b7bc14fe50ecc310e2
liangzhimy <liangzhimy@gmail.com>
2017-09-27 16:03:47 +0800

    Swift draw circle process

--- DrawCircleSwift/ViewController.swift
+++ DrawCircleSwift/ViewController.swift
@@ -8,12 +8,24 @@
 
 import Cocoa
 
+extension Double {
+    var degreesToRadians: Double { return Double(self) * .pi / 180 }
+}
+
 class ViewController: NSViewController {
 
+    @IBOutlet weak var imageView: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        var process = 0.0
+        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { (timer) in
+            process += 0.01
+            if let image = self.__renderCircleImage(radius: 150, process: CGFloat(process), strokeWidth: 2) {
+                self.imageView.image = image
+            }
+        }
     }
 
     override var representedObject: Any? {
@@ -21,7 +33,33 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
+    func __renderCircleImage(radius: CGFloat, process: CGFloat, strokeWidth: CGFloat) -> NSImage? {
+        let imageSize = NSMakeSize(radius * 2, radius * 2)
+        let offscreenRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(imageSize.width), pixelsHigh: Int(imageSize.height), bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false, colorSpaceName: NSColorSpaceName.deviceRGB, bitmapFormat: NSBitmapImageRep.Format.alphaFirst, bytesPerRow: 0, bitsPerPixel: 0)
+        let g = NSGraphicsContext(bitmapImageRep: offscreenRep!)
+        NSGraphicsContext.saveGraphicsState()
+        NSGraphicsContext.current = g
+        let ctx = g?.cgContext
+        if (strokeWidth > 0.0) {
+            ctx?.setLineWidth(strokeWidth)
+            ctx?.addArc(center: CGPoint(x: radius, y: radius), radius: radius - strokeWidth * 0.5, startAngle: 0, endAngle:  CGFloat(360.degreesToRadians), clockwise: true)
+            ctx?.drawPath(using: CGPathDrawingMode.stroke)
+        }
+        
+        ctx?.move(to: CGPoint(x: radius, y: radius))
+        let endAngle = CGFloat(Double(90 - process * 360.0).degreesToRadians)
+        let startAngle = CGFloat(Double(90).degreesToRadians)
+        ctx?.addArc(center: CGPoint(x: radius, y: radius), radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        ctx?.drawPath(using: CGPathDrawingMode.fill)
+        
+        NSGraphicsContext.restoreGraphicsState()
+        if let data = offscreenRep?.representation(using: NSBitmapImageRep.FileType.png, properties: [:]) {
+            return NSImage(data: data)
+        }
+        
+        return nil
+    }
 
 }
 
