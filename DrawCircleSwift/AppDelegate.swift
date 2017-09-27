3c766d9e98c265437380bd5f6c33d4b5b9137f20
liangzhimy <liangzhimy@gmail.com>
2017-09-27 15:24:52 +0800

    Initial Commit

---
+++ DrawCircleSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DrawCircleSwift
+//
+//  Created by liangzhimy on 2017/9/27.
+//  Copyright © 2017年 laig. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DrawCircleSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DrawCircleSwift
+//
+//  Created by liangzhimy on 2017/9/27.
+//  Copyright © 2017年 laig. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
